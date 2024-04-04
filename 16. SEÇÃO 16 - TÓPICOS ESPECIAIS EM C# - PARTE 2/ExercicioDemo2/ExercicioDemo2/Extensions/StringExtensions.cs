e6099c25b95d137ff0089ad681f6d9c0c664eb4d
Rachel Silva <rachel.silva@microware.com.br>
2024-04-04 11:31:55 -0300

    Resolvendo exercício 'Demo 2' da seção 16 com extensions

---
+++ 16. SEÇÃO 16 - TÓPICOS ESPECIAIS EM C# - PARTE 2/ExercicioDemo2/ExercicioDemo2/Extensions/StringExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace System
+{
+    static class StringExtensions
+    {
+        public static string Cut(this string thisString, int num)
+        {
+            if (thisString.Length > 10)
+            {
+                string nova = thisString.Substring(0, num);
+                return nova + "...";
+            }
+            else return thisString;
+        }
+    }
+}
---
+++ 16. SEÇÃO 16 - TÓPICOS ESPECIAIS EM C# - PARTE 2/ExercicioDemo2/ExercicioDemo2/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace System
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = "Good morning!";
+            string explain = "This program limits a given string to 10 caracters by using Extensios. The class 'String', from the method 'Cut' is responsible " +
+                "for that.";
+
+            Console.WriteLine(s1.Cut(10));
+            Console.WriteLine(explain.Cut(10));
+
+            string s2 = "\nBom dia!";
+            string explica = "Esse programa limita uma dada string a 10 caracteres através do uso de Extensions. O responsável por isso é o método 'Cut'," +
+                "da classe 'String'.";
+
+            Console.WriteLine(s2.Cut(10));
+            Console.WriteLine(explica.Cut(10));
+
+        }
+    }
+}
