ba06863ece85d3b29b060e27fd648b0bbc1baf3c
Rachel Silva <rachel.silva@microware.com.br>
2024-04-04 11:55:56 -0300

    Mudando nome da pasta

---
+++ 1. ESTRUTURA CONDICIONAL/Hello World/ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System; // referência para o namespace chamado System. namespace padrão do
+//.NET que contém funções básicas de manipulação de programa.
+
+namespace PrimeiroProjetoUdemyC
+{
+    internal class Program //todo código-fonte escrito em C# tem que
+                           // estar dentro de uma classe
+    {
+        static void Main(string[] args) //entry point. onde a execução começa
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
