cc092a937f22cc43027ce5c3d784b3dcdd1f0398
Rachel Silva <rachel.silva@microware.com.br>
2024-04-04 12:03:49 -0300

    Tentando organizar a visualização das pastas

---
+++ b. WHILE/Exercicio 1 - While/Exercicio 1 - While/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace exercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int senha_certa = 2002;
+            int senha;
+            Console.Write("Insira a senha: ");
+            senha = int.Parse(Console.ReadLine());
+            while (senha != senha_certa)
+            {
+                Console.WriteLine("Senha Inválida");
+                Console.Write("Insira a senha: ");
+                senha = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Acesso Permitido");
+        }
+    }
+}
