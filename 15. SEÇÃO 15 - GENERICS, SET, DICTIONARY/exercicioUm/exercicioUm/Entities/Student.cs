535d7b694a273962b17e79b3ddd28e3bf0a8fdf4
Rachel Silva <rachel.silva@microware.com.br>
2024-04-04 09:30:45 -0300

    Solucionando exercicio proposto da seção 14

---
+++ 15. SEÇÃO 15 - GENERICS, SET, DICTIONARY/exercicioUm/exercicioUm/Entities/Student.cs
@@ -0,0 +1,27 @@
+﻿namespace Curso
+{
+    internal class Student
+    {
+        public int ID { get; set; }
+
+        public Student(int iD)
+        {
+            ID = iD;
+        }
+
+        public override int GetHashCode()
+        {
+            return ID.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Student))
+            {
+                return false;
+            }
+            Student? other = obj as Student;
+            return ID.Equals(other.ID);
+        }
+    }
+}
---
+++ 15. SEÇÃO 15 - GENERICS, SET, DICTIONARY/exercicioUm/exercicioUm/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<Student> student = new HashSet<Student>();
+
+            for (int i = 0; i < 3; i++)
+            {
+                char curso = 'x';
+                if (i is 0)
+                    curso = 'A';
+
+                else if (i is 1)
+                    curso = 'B';
+
+                else if (i is 2)
+                    curso = 'C';
+
+                Console.Write($"How many students for course {curso}? ");
+                int num = int.Parse(Console.ReadLine());
+
+                try
+                {
+                    for (int a = 0; a < num; a++)
+                    {
+                        int id = int.Parse(Console.ReadLine());
+                        student.Add(new Student(id));
+                    }
+                }
+                catch (IOException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+            Console.WriteLine("Total students: " + student.Count);
+
+        }
+    }
+}
