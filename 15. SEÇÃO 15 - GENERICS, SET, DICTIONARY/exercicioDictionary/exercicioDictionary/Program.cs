9c2a8b5573806079d8585b1d4176ce952256a460
Rachel Silva <rachel.silva@microware.com.br>
2024-04-04 10:22:47 -0300

    Solucionando exercício proposto sobre dicionário

---
+++ 15. SEÇÃO 15 - GENERICS, SET, DICTIONARY/exercicioDictionary/exercicioDictionary/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Exercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter file full path: ");
+            string path = Console.ReadLine();
+            Dictionary<string, int> election = new Dictionary<string, int>();
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string[] line = sr.ReadLine().Split(',');
+                        string name = line[0];
+                        int number = Convert.ToInt32(line[1]);
+
+                        if (!election.ContainsKey(name))
+                        {
+                            election.Add(name, number);
+                            election[name] = number;
+                        }
+                        else
+                            election[name] += number;
+                    }
+                }
+                foreach (KeyValuePair<string, int> pair in election)
+                {
+                    Console.WriteLine(pair.Key + ": " + pair.Value);
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+    }
+}
